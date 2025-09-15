80a2df1be31b27a4e35d14ff0ea82ef86dc07037
khusnan21 <khusnan21@gmail.com>
2025-09-15 17:34:06 +0700

    Update MainActivity.java

--- app/src/main/java/com/nkjayanet/app/MainActivity.java
+++ app/src/main/java/com/nkjayanet/app/MainActivity.java
@@ -3,7 +3,7 @@ package com.nkjayanet.app;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.File;
@@ -17,73 +17,38 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         WebView webView = new WebView(this);
         setContentView(webView);
 
-        // Extract php-cgi binary
-        File binDir = new File(getFilesDir(), "php");
-        if (!binDir.exists()) binDir.mkdirs();
-        File phpCgi = new File(binDir, "php-cgi");
-        if (!phpCgi.exists()) {
-            copyAsset("php/php-cgi", phpCgi);
-            phpCgi.setExecutable(true);
+        // Extract PHP binary
+        File filesDir = getFilesDir();
+        File phpBinary = new File(filesDir, "php");
+        extractAsset("php/php", phpBinary);
+        phpBinary.setExecutable(true);
+
+        // Extract mikhmonv3 folder
+        File mikhmonDir = new File(filesDir, "mikhmonv3");
+        if (!mikhmonDir.exists()) {
+            mikhmonDir.mkdirs();
+            copyAssetFolder("mikhmonv3", mikhmonDir);
         }
 
-        // Extract mikhmonv3
-        File wwwDir = new File(getFilesDir(), "mikhmonv3");
-        if (!wwwDir.exists()) {
-            copyAssetFolder("mikhmonv3", wwwDir.getAbsolutePath());
-        }
-
-        // Start HTTP server via php-cgi
+        // Start PHP server
         try {
-            phpProcess = new ProcessBuilder(
-                    phpCgi.getAbsolutePath(),
-                    "-b", "127.0.0.1:8080",
-                    "-c", getFilesDir().getAbsolutePath()
-            )
-            .redirectErrorStream(true)
-            .start();
+            String cmd = phpBinary.getAbsolutePath() +
+                    " -S 127.0.0.1:8080 -t " + mikhmonDir.getAbsolutePath();
+            phpProcess = Runtime.getRuntime().exec(cmd);
+            Toast.makeText(this, "PHP server started", Toast.LENGTH_SHORT).show();
         } catch (Exception e) {
             e.printStackTrace();
+            Toast.makeText(this, "Failed to start PHP server", Toast.LENGTH_LONG).show();
         }
 
+        // Setup WebView
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("http://127.0.0.1:8080/mikhmonv3/");
-    }
-
-    private void copyAsset(String assetPath, File outFile) {
-        try (InputStream in = getAssets().open(assetPath);
-             FileOutputStream out = new FileOutputStream(outFile)) {
-            byte[] buf = new byte[4096];
-            int len;
-            while ((len = in.read(buf)) > 0) {
-                out.write(buf, 0, len);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void copyAssetFolder(String assetFolder, String destPath) {
-        try {
-            String[] list = getAssets().list(assetFolder);
-            File dir = new File(destPath);
-            if (!dir.exists()) dir.mkdirs();
-            for (String name : list) {
-                String assetFilePath = assetFolder + "/" + name;
-                String outFilePath = destPath + "/" + name;
-                String[] sub = getAssets().list(assetFilePath);
-                if (sub != null && sub.length > 0) {
-                    copyAssetFolder(assetFilePath, outFilePath);
-                } else {
-                    copyAsset(assetFilePath, new File(outFilePath));
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        webView.loadUrl("http://127.0.0.1:8080/");
     }
 
     @Override
@@ -91,6 +56,40 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         if (phpProcess != null) {
             phpProcess.destroy();
+        }
+    }
+
+    // Extract single file from assets
+    private void extractAsset(String assetName, File outFile) {
+        try (InputStream in = getAssets().open(assetName);
+             FileOutputStream out = new FileOutputStream(outFile)) {
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = in.read(buffer)) != -1) {
+                out.write(buffer, 0, read);
+            }
+            out.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Recursive copy folder from assets
+    private void copyAssetFolder(String assetFolder, File dest) {
+        try {
+            String[] assets = getAssets().list(assetFolder);
+            if (assets.length == 0) {
+                // It's a file
+                extractAsset(assetFolder, dest);
+            } else {
+                if (!dest.exists()) dest.mkdirs();
+                for (String asset : assets) {
+                    copyAssetFolder(assetFolder + "/" + asset,
+                            new File(dest, asset));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
